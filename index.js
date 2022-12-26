39f197039806a633214829bc7fe847d450eb59d9
Thiem Duong <thiem.duong@mgisolutions.com>
2022-12-26 08:57:58 +0700

    fix issue when loop with replaceChild

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ function convertTableToGrid(htmlStr) {
   richTextTemplate.classList.add('richText');
   const htmlRemovedEmpty = htmlStr.replace(/<p><\/p>/gi, '');
   richTextTemplate.innerHTML = htmlRemovedEmpty;
-  const table = richTextTemplate.getElementsByTagName('table');
-  for (const item of table) {
-    if (!item.getElementsByTagName('th').length) {
-      const colCounter = item.getElementsByTagName('td').length;
+  const tables = richTextTemplate.getElementsByTagName('table')
+  for (let index = tables.length - 1; index > -1 ; index--) {
+    if (!tables[index].getElementsByTagName('th').length) {
+      const colCounter = tables[index].getElementsByTagName('td').length;
       const div = document.createElement('div');
       div.classList.add('grid-table', 'grid', 'grid-cols-1', `sm:grid-cols-${colCounter}`, 'gap-4');
-      for (const tData of item.getElementsByTagName('td')) {
+      const tdElements = tables[index].getElementsByTagName('td')
+      for (let index = 0; index < tdElements.length; index++) {
         const divCol = document.createElement('div');
         divCol.classList.add('table-col');
-        divCol.innerHTML = tData?.innerHTML;
+        divCol.innerHTML = tdElements[index].innerHTML;
         div.append(divCol);
       }
-      item?.parentNode?.replaceChild(div, item);
+      richTextTemplate.replaceChild(div, tables[index]);
     }
   }
   return richTextTemplate.outerHTML;
